b1cccbddb6df6e62d92bbb718be5fb33232a0aac
Gaurav <aadeezcity@gmail.com>
2021-02-23 15:55:05 +0530

    basic structure has been created

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let td = document.getElementById("data");
-
+let temp = ''
 function fetchAllData() {
     fetch("http://192.168.1.10:80/api/users")
     .then(response=> {
@@ -8,8 +8,42 @@ function fetchAllData() {
            throw Error("error occoucred")
        }     
        return response.json();
-    }).then(data => console.log(data));
+    }).then( (element) => {
+        
+        
+    let arr = element.result
+   x=  console.log(arr)
+
+    arr.forEach(e => {
+       temp += `
+       <tr>
+       <td>${e.id}</td>
+       <td>${e.name}</td>
+       <td> 
+       <ul class="btnGroup">
+       <li class="view">
+       <button type="button" class="btn btn-primary" onClick="show">View</button>
+       </li>
+       <li class="Delete">
+       <button type="button" class="btn btn-danger">Delete</button>
+       </li>
+       <li class="EDIT">
+       <button type="button" class="btn btn-info">Update</button>
+       </li>
+
+   </ul> </td>
+       </tr>
+       `
+    });
+
+    td.innerHTML = temp;
+       
+        
+    })
+   
+    
+}
+    
   
-  }
   
   fetchAllData()
